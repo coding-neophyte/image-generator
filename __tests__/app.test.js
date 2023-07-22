f6060056e4b14d08304024367298850b052f13c4
Julius Franklin <jjfranklin88@outlook.com>
2023-07-22 11:10:13 -0400

    fixing test for CI

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -24,10 +24,10 @@ describe('Testing User Routes', () => {
   it('Sign Up Test', async () => {
     const response = await agent.post('/auth/register').send(fakeUser);
 
-    expect(response.statusCode).toBe(200);
-    expect(response.body.email).toBe(fakeUser.email);
-    expect(response.body.name).toBe(fakeUser.name);
-    expect(response.body.username).toBe(fakeUser.username);
+    expect(response.statusCode).toEqual(200);
+    expect(response.body.email).toEqual(fakeUser.email);
+    expect(response.body.name).toEqual(fakeUser.name);
+    expect(response.body.username).toEqual(fakeUser.username);
   });
 
   it('Sign In Test', async () => {
@@ -37,7 +37,7 @@ describe('Testing User Routes', () => {
       password: fakeUser.password
     });
 
-    expect(response.statusCode).toBe(200);
+    expect(response.statusCode).toEqual(200);
     expect(response.body).toEqual({ message: 'Signed In' });
   });
 
@@ -49,7 +49,7 @@ describe('Testing User Routes', () => {
     });
     const response = await agent.delete('/auth/logout');
 
-    expect(response.statusCode).toBe(204);
+    expect(response.statusCode).toEqual(204);
     expect(response.body).toEqual({});
   });
 });
